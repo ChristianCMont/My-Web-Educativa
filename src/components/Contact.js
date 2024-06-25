0b109c9a6f202caa51dd4a72207de205de512798
ChristianCMont <christmont3@gmail.com>
2024-06-25 17:34:32 -0500

    contact 1.3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ return (
       <div class="header-content container">
 
         <div class="header-txt" >
-          <h1><span>¡Inscríbete ya</span> y sera tu camino al éxito!
+          <h1><span>¡contact</span> y sera tu camino al éxito!
           </h1>
           <p>
             Descubre una nueva forma de aprender con nuestros cursos interactivos en vivo. Disfruta de una experiencia educativa única desde la comodidad de tu hogar. ¡No te pierdas esta oportunidad de crecer y desarrollar nuevas habilidades con la guía de expertos en tiempo real!
